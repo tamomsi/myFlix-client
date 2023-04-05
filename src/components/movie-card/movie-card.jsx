5c5f184104c23200d83024b0f6852f20fcaa3112
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-04-05 21:06:47 +0200

    import propTypes
    If applied, this commit will import prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
       <div
@@ -8,5 +10,24 @@ export const MovieCard = ({ movie, onMovieClick }) => {
         {movie.title}
       </div>
     );
+  };
+  
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      Title: PropTypes.string,
+      Description: PropTypes.string.isRequired,
+    image: PropTypes.string.isRequired,
+    Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired,
+      }).isRequired,
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired,
+        Birth: PropTypes.string,
+        Death: PropTypes.string,
+    })
+}).isRequired,
+    onMovieClick: PropTypes.func.isRequired
   };
   
