9b0da965e5d13d9a53305c34d1a6051751829801
tamomsi <122281591+tamomsi@users.noreply.github.com>
2023-04-05 18:02:39 +0200

    remove hardcoded movies
    if applied, this commit will set the initial value of movies to an empty list by deleting the hardcoded movies.

--- dist/index.b4b6dfad.js
+++ dist/index.b4b6dfad.js
@@ -27297,62 +27297,21 @@ var _movieView = require("../movie-view/movie-view");
 var _s = $RefreshSig$();
 const MainView = ()=>{
     _s();
-    const [movies, setMovies] = (0, _react.useState)([
-        {
-            id: 1,
-            title: "Titanic",
-            description: "Titanic is a 1997 American epic romance and disaster film directed, written, produced, and co-edited by James Cameron. Incorporating both historical and fictionalised aspects, it is based on accounts of the sinking of the RMS Titanic and stars Kate Winslet and Leonardo DiCaprio as members of different social classes who fall in love aboard the ship during its ill-fated maiden voyage.",
-            image: "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png?20190508145520",
-            director: "James Francis Cameron",
-            genre: "Romance"
-        },
-        {
-            id: 2,
-            title: "Brokeback Mountain",
-            description: "Brokeback Mountain is a 2005 American neo-Western romantic drama film directed by Ang Lee and produced by Diana Ossana and James Schamus. The film stars Heath Ledger, Jake Gyllenhaal, Anne Hathaway, and Michelle Williams and depicts the complex romantic relationship between two American cowboys, Ennis Del Mar and Jack Twist, in the American West from 1963 to 1983.",
-            image: "https://upload.wikimedia.org/wikipedia/en/a/a1/Brokeback_mountain.jpg?20160109003759",
-            director: "Ang Lee",
-            genre: "Drama"
-        },
-        {
-            id: 3,
-            title: "Carol",
-            description: "Set in New York City during the early 1950s, Carol tells the story of a forbidden affair between an aspiring female photographer and an older woman going through a difficult divorce.",
-            image: "https://upload.wikimedia.org/wikipedia/en/1/10/Carol_film_poster.jpg?20161224155611",
-            director: "Todd Haynes",
-            genre: "Drama"
-        },
-        {
-            id: 4,
-            title: "Home Alone",
-            description: "It is Christmas time and the McCallister family is preparing for a vacation in Paris, France. But the youngest in the family, Kevin (Macaulay Culkin), got into a scuffle with his older brother Buzz (Devin Ratray) and was sent to his room, which is on the third floor of his house. Then, the next morning, while the rest of the family was in a rush to make it to the airport on time, they completely forgot about Kevin, who now has the house all to himself.",
-            image: "https://upload.wikimedia.org/wikipedia/en/7/76/Home_alone_poster.jpg?20170303210933",
-            director: "Chris Columbus",
-            genre: "Family"
-        },
-        {
-            id: 5,
-            title: "The Holiday",
-            description: "The Holiday is a 2006 romantic comedy film written, produced and directed by Nancy Meyers. Coproduced by Bruce A. Block, it was filmed in both California and England and stars Kate Winslet and Cameron Diaz as Iris and Amanda, two lovelorn women from opposite sides of the Atlantic Ocean, who arrange a home exchange to escape heartbreak during the Christmas and holiday season.",
-            image: "https://upload.wikimedia.org/wikipedia/en/6/60/Theholidayposter.jpg?20171216233620",
-            director: "Nancy Meyers",
-            genre: "Comedy"
-        }
-    ]);
+    const [movies, setMovies] = (0, _react.useState)([]);
     const [selectedMovie, setSelectedMovie] = (0, _react.useState)(null);
     if (selectedMovie) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)((0, _movieView.MovieView), {
         movie: selectedMovie,
         onBackClick: ()=>setSelectedMovie(null)
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 58,
+        lineNumber: 12,
         columnNumber: 7
     }, undefined);
     if (movies.length === 0) return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
         children: "The list is empty!"
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 63,
+        lineNumber: 17,
         columnNumber: 12
     }, undefined);
     return /*#__PURE__*/ (0, _jsxDevRuntime.jsxDEV)("div", {
@@ -27363,16 +27322,16 @@ const MainView = ()=>{
                 }
             }, movie.id, false, {
                 fileName: "src/components/main-view/main-view.jsx",
-                lineNumber: 69,
+                lineNumber: 23,
                 columnNumber: 9
             }, undefined))
     }, void 0, false, {
         fileName: "src/components/main-view/main-view.jsx",
-        lineNumber: 67,
+        lineNumber: 21,
         columnNumber: 5
     }, undefined);
 };
-_s(MainView, "eJrcjj6wT1tT5XKZK3EfMIoNOGY=");
+_s(MainView, "jzuc4/Y81KLA8pUTcmWNL/aPH2I=");
 _c = MainView;
 var _c;
 $RefreshReg$(_c, "MainView");
--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -3,53 +3,7 @@ import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "Titanic",
-      description: "Titanic is a 1997 American epic romance and disaster film directed, written, produced, and co-edited by James Cameron. Incorporating both historical and fictionalised aspects, it is based on accounts of the sinking of the RMS Titanic and stars Kate Winslet and Leonardo DiCaprio as members of different social classes who fall in love aboard the ship during its ill-fated maiden voyage.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/1/18/Titanic_%281997_film%29_poster.png?20190508145520",
-      director: "James Francis Cameron",
-      genre: "Romance"
-    },
-    {
-      id: 2,
-      title: "Brokeback Mountain",
-      description: "Brokeback Mountain is a 2005 American neo-Western romantic drama film directed by Ang Lee and produced by Diana Ossana and James Schamus. The film stars Heath Ledger, Jake Gyllenhaal, Anne Hathaway, and Michelle Williams and depicts the complex romantic relationship between two American cowboys, Ennis Del Mar and Jack Twist, in the American West from 1963 to 1983.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/a/a1/Brokeback_mountain.jpg?20160109003759",
-      director: "Ang Lee",
-      genre: "Drama"
-    },
-    {
-      id: 3,
-      title: "Carol",
-      description: "Set in New York City during the early 1950s, Carol tells the story of a forbidden affair between an aspiring female photographer and an older woman going through a difficult divorce.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/1/10/Carol_film_poster.jpg?20161224155611",
-      director: "Todd Haynes",
-      genre: "Drama"
-    },
-    {
-      id: 4,
-      title: "Home Alone",
-      description: "It is Christmas time and the McCallister family is preparing for a vacation in Paris, France. But the youngest in the family, Kevin (Macaulay Culkin), got into a scuffle with his older brother Buzz (Devin Ratray) and was sent to his room, which is on the third floor of his house. Then, the next morning, while the rest of the family was in a rush to make it to the airport on time, they completely forgot about Kevin, who now has the house all to himself.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/7/76/Home_alone_poster.jpg?20170303210933",
-      director: "Chris Columbus",
-      genre: "Family"
-    },
-    {
-      id: 5,
-      title: "The Holiday",
-      description: "The Holiday is a 2006 romantic comedy film written, produced and directed by Nancy Meyers. Coproduced by Bruce A. Block, it was filmed in both California and England and stars Kate Winslet and Cameron Diaz as Iris and Amanda, two lovelorn women from opposite sides of the Atlantic Ocean, who arrange a home exchange to escape heartbreak during the Christmas and holiday season.",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/6/60/Theholidayposter.jpg?20171216233620",
-      director: "Nancy Meyers",
-      genre: "Comedy"
-    }
-  ]);
+    const [movies, setMovies] = useState([]);
 
   const [selectedMovie, setSelectedMovie] = useState(null);
 
